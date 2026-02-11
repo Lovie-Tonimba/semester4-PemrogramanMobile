0ae38d7e463bd447a68c57ff3e2113880f7e8c20
Lovie-Tonimba <jechonialovie@gmail.com>
2026-02-11 20:01:09 +0700

    pertemuan 1 : melakukan percobaan menggunakan dart untuk menjalankan program hello word dan menghitung luas segitiga.

---
+++ dart_basic1/bin/dart_basic1.dart
@@ -0,0 +1,8 @@
+import 'package:dart_basic1/dart_basic1.dart' as dart_basic1;
+
+void main(List<String> arguments) {
+  print('Alas = 4');
+  print('Tinggi = 3');
+  print('Luas Segitiga: ${dart_basic1.luasSegitiga()}!');
+  print('Hello World!: ${dart_basic1.calculate()}!');
+}
---
+++ dart_basic1/lib/dart_basic1.dart
@@ -0,0 +1,7 @@
+int calculate() {
+  return 6 * 7;
+}
+
+double luasSegitiga() {
+  return 4 * 3 / 2;
+}
