0524b997fb629a3e16434bb949b0927e099e5958
Lovie-Tonimba <jechonialovie@gmail.com>
2026-04-15 14:29:50 +0700

    Praktikum 2, praktikum 3, dan praktikum 4 selesai

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -9,54 +9,107 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /* soal 1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /* soal 2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'Wisata Gunung di Batu',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                'Batu, Malang, Indonesia',
-                style: TextStyle(color: Colors.grey[500]),
-              ),
-            ],
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
           ),
-        ),
-        /* soal 3*/
-        Icon(
-        Icons.star,
-          color: Colors.red[500],
-        ),
-        const Text('41'),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
       ],
-    ),
-  );
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Gunung Bromo (2.329 mdpl) adalah gunung berapi aktif ikonik di Jawa Timur, '
+        'bagian dari Taman Nasional Bromo Tengger Semeru (TNBTS), '
+        'yang terkenal dengan pemandangan sunrise spektakuler, '
+        'lautan pasir seluas 10 km², dan kawah aktif.'
+        ' Destinasi ini menawarkan pengalaman wisata alam menakjubkan,  populer dengan Jeep tour, '
+        'mendaki kawah, serta panorama Savana dan Pasir Berbisik.\n\n'
+        'oleh: Lovie Jechonia Tonimba (244107060101)',
+        softWrap: true,
+      ),
+    );
 
     return MaterialApp(
       title: 'Flutter layout: Lovie Jechonia Tonimba 244107060101',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Column(
-          children: [
-            titleSection,
+          appBar: AppBar(
+            title: const Text('Flutter layout demo'),
+          ),
+          body: ListView(
+            children: [
+              Image.asset(
+                'img/bromo.png',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textSection,
             ],
-        )
-      ),
+          )),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
